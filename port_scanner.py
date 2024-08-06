1d4bf976628bc3f21647f6a9a752ff0bde77f78e
lemonheee <162993996+lemonheee@users.noreply.github.com>
2024-08-06 15:04:33 +0200

    Update port_scanner.py
    1

--- port_scanner.py
+++ port_scanner.py
@@ -3,6 +3,19 @@ import socket
 def get_open_ports(target, port_range, verbose = False):
     open_ports = []
 
+  if verbose == True:
+        print("Port    Service")
 
+    for scan_port in range(port_range[0], port_range[1]+1):
+        s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        result = s.connect_ex((target,scan_port))
+        if result ==0:
+            #print(f" port {scan_port} is  open ")
+            open_ports.append ( scan_port )
+            if verbose == True:
+                port_v= scan_port
+                service_v= common_ports.ports_and_services[scan_port]
+                print (port_v, "    ",service_v)
 
     return(open_ports)
