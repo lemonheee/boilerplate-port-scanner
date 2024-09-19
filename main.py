f5e709d51aa2cd1e576dab8b00e8bcc1ef0025ce
lemonheee <162993996+lemonheee@users.noreply.github.com>
2024-09-19 20:18:53 +0200

    Update main.py
    Add messages to improve user experience.
    
    test_module is yet to be fixed

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-# This entrypoint file to be used in development. Start by reading README.md
+his entrypoint file to be used in development. Start by reading README.md
 import port_scanner
 from unittest import main
 
 # Called with URL
+print("Scanning \"www.freecodecamp.org\"...")
 ports = port_scanner.get_open_ports("www.freecodecamp.org", [75, 85])
 print("Open ports:", ports, "\n")
 
 # Called with ip address
+print ("Scanning 104.26.10.78...")
 ports = port_scanner.get_open_ports("104.26.10.78", [8079, 8090])
 print("Open ports:", ports, "\n")
 
 # Verbose called with ip address and no host name returned -- single open port
+print ("Scanning 104.26.10.78...")
 ports = port_scanner.get_open_ports("104.26.10.78", [440, 450], True)
-print(ports, "\n")
+print("Open ports:",ports, "\n")
 
 # Verbose called with ip address and valid host name returned -- single open port
+print ("Scanning 137.74.187.104...")
 ports = port_scanner.get_open_ports("137.74.187.104", [440, 450], True)
-print(ports, "\n")
+print("Open Ports:", ports, "\n")
 
 # Verbose called with host name -- multiple ports returned
+print ("Scanning scanme.nmap.org...")
 ports = port_scanner.get_open_ports("scanme.nmap.org", [20, 80], True)
-print(ports, "\n")
+print("Open ports:" ,ports, "\n")
 
 # Run unit tests automatically
-main(module = "test_module", exit = False)
+#main(module = "test_module", exit = False)
